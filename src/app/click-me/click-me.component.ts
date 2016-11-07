e0102f7658f550edc2d901e7860dc5b1b60698bd
Enda Lee <enda.lee@ittdublin.ie>
2016-11-07 13:35:00 +0000

    Added new component

---
+++ src/app/click-me/click-me.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-click-me',
+  templateUrl: './click-me.component.html',
+  styleUrls: ['./click-me.component.css']
+})
+export class ClickMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
